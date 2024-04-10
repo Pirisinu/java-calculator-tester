95a3734964e81be0ec8a3bb49cbf53c2d72b6bdd
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-10 16:44:56 +0200

    add Tiket constructor controll min value input

--- src/main/java/javaCalculator/Tiket.java
+++ src/main/java/javaCalculator/Tiket.java
@@ -4,9 +4,12 @@ public class Tiket {
     // Attributes
     private int kilometers;
     private int age;
-    // Constructor
 
+    // Constructor
     public Tiket(int kilometers, int age) {
+        if (kilometers <= 0 || age <= 0) {
+            throw new IllegalArgumentException("Kilometers and age must be positive numbers.");
+        }
         this.kilometers = kilometers;
         this.age = age;
     }
