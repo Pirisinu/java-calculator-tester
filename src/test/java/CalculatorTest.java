bf4a93ebfb5a98f24b01d378074e3476675abadb
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-10 16:19:32 +0200

    EDIT in CalculatorTest methods: add general TEST(subtract-divide-multiply)

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -29,24 +29,54 @@ public class CalculatorTest {
         // Controllo con entrambe gli input NaN
         assertThrows(IllegalArgumentException.class, () -> calculator.add(Float.NaN, Float.NaN));
 
-        // Controllo oltre limite max value
-        assertThrows(IllegalArgumentException.class, () -> calculator.add(Float.MAX_VALUE, 1.0f));
-        assertThrows(IllegalArgumentException.class, () -> calculator.add(1.0f, Float.MAX_VALUE));
+//        // Controllo oltre limite max value
+//        assertThrows(IllegalArgumentException.class, () -> calculator.add(Float.MAX_VALUE, 1.0f));
+//        assertThrows(IllegalArgumentException.class, () -> calculator.add(1.0f, Float.MAX_VALUE));
     }
 
     // Test sottrazione
     @Test
     @DisplayName("Subtraction test")
-    public void testSubtract(){}
+    public void testSubtract(){
+         // Controllo con input validi
+        assertEquals(2.0f, calculator.subtract(5.0f, 3.0f), 0.0f);
+
+        // Controllo con un input NaN
+        assertThrows(IllegalArgumentException.class, () -> calculator.subtract(Float.NaN, 3.0f));
+        assertThrows(IllegalArgumentException.class, () -> calculator.subtract( 3.0f,Float.NaN));
+
+        // Controllo con entrambe gli input NaN
+        assertThrows(IllegalArgumentException.class, () -> calculator.subtract(Float.NaN, Float.NaN));
+    }
 
     // Test moltiplicatione
     @Test
     @DisplayName("Multiplication test")
-    public void testMultiply(){}
+    public void testMultiply(){
+         // Controllo con input validi
+        assertEquals(20.0f, calculator.multiply(10.0f, 2.0f), 0.0f);
+
+        // Controllo con un input NaN
+        assertThrows(IllegalArgumentException.class, () -> calculator.multiply(Float.NaN, 3.0f));
+        assertThrows(IllegalArgumentException.class, () -> calculator.multiply( 3.0f,Float.NaN));
+
+        // Controllo con entrambe gli input NaN
+        assertThrows(IllegalArgumentException.class, () -> calculator.multiply(Float.NaN, Float.NaN));
+    }
 
     // Test divisione
     @Test
     @DisplayName("Division test")
-    public void testDivide(){}
+    public void testDivide(){
+         // Controllo con input validi
+        assertEquals(2.0f, calculator.divide(10.0f, 5.0f), 0.0f);
+
+        // Controllo con un input NaN
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(Float.NaN, 3.0f));
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide( 3.0f,Float.NaN));
+
+        // Controllo con entrambe gli input NaN
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(Float.NaN, Float.NaN));
+    }
 
 }
