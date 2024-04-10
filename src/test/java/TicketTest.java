6d48aa02cabaa401c1e05161285ccb39fa648c4c
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-10 17:08:38 +0200

    add instance of Ticket + testtestCalculatePrice

--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,2 +1,14 @@
+import javaCalculator.Ticket;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class TicketTest {
+    private Ticket ticket;
+
+
+    @Test
+    @DisplayName("Ticket tester")
+    void testCalculatePrice() {
+
+    }
 }
