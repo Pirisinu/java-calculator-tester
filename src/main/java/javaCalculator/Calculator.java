779769f0e609f45ee44e8696cc508c2f63110233
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-10 15:13:05 +0200

    add Calculator method summ

--- src/main/java/javaCalculator/Calculator.java
+++ src/main/java/javaCalculator/Calculator.java
@@ -1,4 +1,12 @@
 package javaCalculator;
 
 public class Calculator {
+
+    public float add(Float num1, Float num2){
+        if (num1.isNaN()|| num2.isNaN()){
+            throw new IllegalArgumentException("Invalid argument");
+        } else return num1 + num2;
+    }
+
+
 }
