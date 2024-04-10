42aa16fe345b00385724d69529384da5c81424ef
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-10 16:07:50 +0200

    add Calculator methods: subtract-divide-multiply

--- src/main/java/javaCalculator/Calculator.java
+++ src/main/java/javaCalculator/Calculator.java
@@ -9,4 +9,28 @@ public class Calculator {
         return num1 + num2;
     }
 
+    public float subtract(float num1, float num2) {
+        if (Float.isNaN(num1) || Float.isNaN(num2)){
+            throw new IllegalArgumentException("Invalid argument: NaN");
+        }
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) {
+        if (Float.isNaN(num1) || Float.isNaN(num2)){
+            throw new IllegalArgumentException("Invalid argument: NaN");
+        }
+        if (num2 == 0.0f) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        if (Float.isNaN(num1) || Float.isNaN(num2)){
+            throw new IllegalArgumentException("Invalid argument: NaN");
+        }
+        return num1 * num2;
+    }
+
 }
