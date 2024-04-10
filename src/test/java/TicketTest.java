9866557713af9066da3838e6c1cf3cc5b8c9a841
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-10 17:11:11 +0200

    add beforeEach createTicketInstance

--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,10 +1,16 @@
 import javaCalculator.Ticket;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TicketTest {
     private Ticket ticket;
 
+    // Istanza vuota per ogni Test
+    @BeforeEach
+    void createTicketInstance() {
+        ticket = null;
+    }
 
     @Test
     @DisplayName("Ticket tester")
