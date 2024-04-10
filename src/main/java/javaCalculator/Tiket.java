f53b9d6e254da56ea9d7063dc4053cae46707ce8
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-10 16:34:15 +0200

    add Tiket constructor

--- src/main/java/javaCalculator/Tiket.java
+++ src/main/java/javaCalculator/Tiket.java
@@ -5,6 +5,12 @@ public class Tiket {
     private int kilometers;
     private int age;
     // Constructor
+
+    public Tiket(int kilometers, int age) {
+        this.kilometers = kilometers;
+        this.age = age;
+    }
+
     // Getter e Setter
     // Methods
 
